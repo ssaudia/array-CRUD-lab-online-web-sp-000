3e67cb515fd24c69f00d247599b1a066892743f6
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-27 01:08:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+  #array[0]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  #array[-1]
+  #array.last
+  array[|]
 end
